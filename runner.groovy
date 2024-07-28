e42525189aa9787e1fad585e66ae0fcdba7d84d4
AWKrol <92876406+AWKrol@users.noreply.github.com>
2024-07-28 22:41:33 +0300

    first commit

---
+++ runner.groovy
@@ -0,0 +1,38 @@
+timeout(60){
+    node("maven"){
+        prepareConfig()
+        wrap([$class: 'BuildUser']) {
+            currentBuild.description = "User: $BUILD_USER"
+        }
+        stage("Checkout") {
+            checkout git-api-tests
+            git branch: $REFSPEC, url: "https://github.com/AWKrol/rest_assured_tests"
+        }
+
+        def jobs = [:]
+        env.TESTS_TYPE.each(v -> {
+            jobs["$v"] = node("maven") {
+                stage("Running test $v") {
+                    triggerJob($v)
+                }
+            }
+        })
+
+        parallel jobs
+
+        stage("Publish allure report") {
+            sh "mkdir -p ./allure-results"
+            dir("allure-results") {
+                jobs.each(k, v -> {
+                    copyArtifacts projectName: $v, selector: specific(v.getBuildNumber())
+                })
+            }
+
+            allure([
+                    disabled:true,
+                    results:["$pwd/allure-results"]
+            ])
+        }
+
+    }
+}
